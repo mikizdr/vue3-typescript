8a3877e2fde8d22c2b04f1ae2bd71c52bf00a04a
Miroslav Zdravkovic <miroslav.zdravkovic@gmail.com>
2021-02-27 18:11:57 +0100

    Header and Nav components

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,9 @@
 <template>
-  <header
-    class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow"
-  >
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"
-      >Company name</a
-    >
-    <button
-      class="navbar-toggler position-absolute d-md-none collapsed"
-      type="button"
-      data-bs-toggle="collapse"
-      data-bs-target="#sidebarMenu"
-      aria-controls="sidebarMenu"
-      aria-expanded="false"
-      aria-label="Toggle navigation"
-    >
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <input
-      class="form-control form-control-dark w-100"
-      type="text"
-      placeholder="Search"
-      aria-label="Search"
-    />
-    <ul class="navbar-nav px-3">
-      <li class="nav-item text-nowrap">
-        <a class="nav-link" href="#">Sign out</a>
-      </li>
-    </ul>
-  </header>
+  <Header />
 
   <div class="container-fluid">
     <div class="row">
-      <nav
-        id="sidebarMenu"
-        class="col-md-3 col-lg-2 d-md-block bg-light sidebar collapse"
-      >
-        <div class="position-sticky pt-3">
-          <ul class="nav flex-column">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">
-                Dashboard
-              </a>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Nav />
 
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
         <div class="table-responsive">
@@ -73,6 +32,18 @@
     </div>
   </div>
 </template>
+
+<script>
+import Nav from "@/components/Nav";
+import Header from "@/components/Header";
+
+export default {
+  components: {
+    Nav,
+    Header,
+  },
+};
+</script>
 
 <style>
 body {
